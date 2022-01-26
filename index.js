162a4d6ef4a3ce01a7f37b56737b36afc6a3dcc4
Jeslin <jeslinjerome@gmail.com>
2022-01-27 00:38:52 +0530

    cors changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express');
+var cors = require('cors');
 
 connectToMongo();
 const app = express();
 const port = 5000;
 
+app.use(cors())
 app.use(express.json())
 
 //endpoint - Available Routes
@@ -14,5 +16,3 @@ app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
